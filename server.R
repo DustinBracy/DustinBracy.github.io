bf723db8ef8d86256a1352b079cc70ef878e1643
DustinBracy <34081222+DustinBracy@users.noreply.github.com>
2019-11-13 22:50:35 -0600

    updated server & ui. Added pre-merged beer.csv

--- server.R
+++ server.R
@@ -1,10 +1,11 @@
 library(shiny)
 library(tidyverse)
-#library(rsconnect)
+library(shinyjs)
+library(rsconnect)
 
 # Define server logic required to draw a histogram ----
+
 server <- function(input, output) {
-  
   # Histogram of the Old Faithful Geyser Data ----
   # with requested number of bins
   # This expression that generates a histogram is wrapped in a call
@@ -17,34 +18,56 @@ server <- function(input, output) {
   #require file
  # req(input$file)
   #Get beer data
+  #getData <- reactive({
+   # if( is.null(input$beerFile))
+    #  return(NULL)
+   # beerdata <- read.csv(req(input$beerFile$datapath), header=T)
+   # })
   
   #prints filename after upload
   output$FileName <- renderPrint({
-    str(input$file)
+    str(input$BeerFile)
   })
   
-  output$PlotTypeValue <- renderPrint({str(input$RadioPlotType)})
+  #output$PlotTypeValue <- renderPrint({str(input$RadioPlotType)})
   
+  #output$states <-renderPrint(input$states)
+ # output$states <- renderUI({
+ #   beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+ #   stateList <- unique(beerdata$State) #%>% arrange(State)
+    #selectInput("states", choices = stateList, label = h3("State Filter"))
+    
+ # })
+  
+#  observeEvent(getData(), {
+#    beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+ #   stateList <- unique(beerdata$State) #%>% arrange(State)
+ #   updateSelectInput("states", choices = stateList)
+ # })
 
   #render head of the data, can delete
-  output$FileContents <- renderTable(
-    head(read.csv(req(input$file$datapath), header=T))
-    )
+  #output$FileContents <- renderTable(
+   # head(read.csv(req(input$BeerFile$datapath), header=T))
+    #head(getData)
+   # )
   
   #render IBU Plots
   output$IBUplot <- renderPlot({
-    beerdata <- read.csv(req(input$file$datapath), header=T)
-    x <- beerdata %>% select(IBU) %>% filter(!is.na(IBU))
+   # req(input$RadioPlotType != "scatterplot")
+   # show("IBUplot")
+   # hide("ScatterPlot")
+    beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+    beerdata <- beerdata %>% filter(!is.na(IBU)) %>% filter(beerdata$state %in% input$states)
+    x <- beerdata %>% select(IBU) 
     x <- as.numeric(x$IBU)
     IBUbins <- seq(min(x), max(x), length.out = input$IBUbins + 1)
     
     if (input$RadioPlotType == "boxplot") {
-      boxplot(x, col = "#75AADB", border = "white",
+      boxplot(x, col = "#75AADB", border = "black",
               xlab = "Beer Distribution",
               ylab = "IBU Rating",
               main = "Boxplot of International Bitterness Units (IBU)")
     } else {
-    
       hist(x, breaks = IBUbins, col = "#75AADB", border = "white",
            xlab = "IBU Rating",
            ylab = "Number of Beers",
@@ -54,17 +77,20 @@ server <- function(input, output) {
   
   #render ABV Plots
   output$ABVplot <- renderPlot({
-    beerdata <- read.csv(req(input$file$datapath), header=T)
-    x <- beerdata %>% select(ABV) %>% filter(!is.na(ABV))
+   # req(input$RadioPlotType != "scatterplot")
+   # show("ABVplot")
+   # hide("ScatterPlot")
+    beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+    x <- beerdata %>% select(ABV) %>% filter(!is.na(ABV)) %>% filter(beerdata$state %in% input$states)
     x <- as.numeric(x$ABV)
     ABVbins <- seq(min(x), max(x), length.out = input$ABVbins + 1)
     
-
     if (input$RadioPlotType == "boxplot") {
-    boxplot(x, col = "#75AADB", border = "white",
+    boxplot(x, col = "#75AADB", border = "black",
             xlab = "Beer Distribution",
             ylab = "ABV Rating",
             main = "Boxplot of Alcohol by Volume (ABV)")
+
     } else {
       hist(x, breaks = ABVbins, col = "#75AADB", border = "white",
            xlab = "ABV Rating",
@@ -72,10 +98,61 @@ server <- function(input, output) {
            main = "Histogram of Alcohol by Volume (ABV)")
     }
   })
+  
+  #render Scatter Plot
+  output$ScatterPlot <- renderPlot({
+   # req(input$RadioPlotType == "scatterplot")
+   # hide("ABVplot")
+   # hide("IBUplot")
+   # show("ScatterPlot")
+    beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+    Brewdata <- beerdata %>% filter(!is.na(IBU) & !is.na(ABV) & !is.na(Style)) %>% filter(beerdata$state %in% input$states)
+    plot <- ggplot(Brewdata, aes(x=IBU, y= ABV)) + 
+      geom_point() + 
+      theme_classic() +
+      theme(axis.text.x=element_text(size=rel(1.0))) +
+      labs(x="IBU",y="ABV") +
+      ggtitle("Correlation between IBU and ABV")
+    
+    if(input$line == T){
+      plot + geom_smooth(method=lm)
+      } 
+    else {
+      plot
+      }
+    })
+
+
+# Calculate Top 10 Cities with the most Breweries
+  output$CityPlot <- renderPlot({
+    beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+    Brewdata <- beerdata %>% filter(!is.na(IBU) & !is.na(ABV) & !is.na(Style)) %>% filter(beerdata$state %in% input$states)
+    TopCities <- Brewdata %>% mutate(City = paste0(City, ', ', state)) %>% group_by(City) %>% 
+      summarise(Breweries = n()) %>% arrange(desc(Breweries)) %>% top_n(10)
+    
+    ggplot(data=TopCities, mapping=aes(x=City, y=Breweries)) + geom_col(color="white",fill="#75AADB") +
+      ggtitle("Top 10 Cities with Most Breweries") +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 60, vjust=1, hjust=1))
+
+  })
+
+  # Calculate Top 10 Breweries with the most original beers
+  output$BrewPlot <- renderPlot({
+    beerdata <- read.csv(req(input$BeerFile$datapath), header=T)
+    Brewdata <- beerdata %>% filter(!is.na(IBU) & !is.na(ABV) & !is.na(Style)) %>% filter(beerdata$state %in% input$states)
+    
+    TopBreweries <- Brewdata %>% mutate(Brewery = paste0(Brewery,'\n', City, ', ', state)) %>% 
+      group_by(Brewery) %>% summarise(Beers = n()) %>% arrange(desc(Beers)) %>% top_n(10)
+    ggplot(data=TopBreweries, mapping=aes(x=Brewery, y=Beers)) + geom_col(color="white",fill="#75AADB") +
+      theme_classic() +
+      ggtitle("Top 10 Breweries with Most Original Beers") +
+      theme(axis.text.x = element_text(angle = 60, vjust=1, hjust=1))
+
+  
+  })
 }
 
-#deployApp()
+deployApp()
 
 #shinyApp(ui, server)
-
-
--- ui.R
+++ ui.R
@@ -1,7 +1,8 @@
 library(shiny)
-
+library(shinyjs)
+library(rsconnect)
 ui <- fluidPage(
-  
+  useShinyjs(),
   # App title ----
   titlePanel("Hello Shiny!"),
   
@@ -10,13 +11,17 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      # Read Beer File:
-      fileInput("file", label = h3("Upload Beer Data")),
-
-      # Input: radio buttons for boxplot/histogram
+     
+       # Read Beer File:
+      fileInput("BeerFile", label = h3("Upload Beer Data")),
+     
+       # Input: radio buttons for boxplot/histogram
       radioButtons("RadioPlotType", label = h3("Plot Type"),
-                   choices = list("Histogram" = 'histogram', "Boxplot" = 'boxplot'), 
+                   choices = list("Histogram" = 'histogram', "Boxplot" = 'boxplot'),# "Scatterplot"='scatterplot'), 
                    selected = 'histogram'),
+     
+       # Input: Regression line boolean
+      checkboxInput("line", label = "Include Regression Line", value = TRUE),
       
       # Input: Slider for the number of IBU bins
       sliderInput(inputId = "IBUbins",
@@ -33,15 +38,20 @@ ui <- fluidPage(
                   value = 30),
 
       # Input: State picker 
-      selectInput("states", label = h3("State Filter"), choices = NULL, multiple = T),
+      selectInput("states", label = h3("State Filter"), multiple =T, 
+                  choices = list("AK"="AK","AL"="AL","AR"="AR","AZ"="AZ","CA"="CA","CO"="CO","CT"="CT","DC"="DC","DE"="DE","FL"="FL","GA"="GA","HI"="HI","IA"="IA","ID"="ID","IL"="IL","IN"="IN","KS"="KS","KY"="KY","LA"="LA","MA"="MA","MD"="MD","ME"="ME","MI"="MI","MN"="MN","MO"="MO","MS"="MS","MT"="MT","NC"="NC","ND"="ND","NE"="NE","NH"="NH","NJ"="NJ","NM"="NM","NV"="NV","NY"="NY","OH"="OH","OK"="OK","OR"="OR","PA"="PA","RI"="RI","SC"="SC","TN"="TN","TX"="TX","UT"="UT","VA"="VA","VT"="VT","WA"="WA","WI"="WI","WV"="WV","WY"="WY"),
+                  selected = c("AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","TN","TX","UT","VA","VT","WA","WI","WV","WY")
+                  ),
+
+      
       
       hr(),
-      fluidRow(column(1, verbatimTextOutput("FileName"))),
-      fluidRow(column(2, verbatimTextOutput("PlotTypeValue"))),
-      fluidRow(column(3, verbatimTextOutput("states"))),
-      fluidRow(column(4, verbatimTextOutput("IBUbins"))),
-      fluidRow(column(5, verbatimTextOutput("ABVbins")))
-      
+     # fluidRow(column(12, verbatimTextOutput("FileName"))),
+     # fluidRow(column(12, verbatimTextOutput("PlotTypeValue"))),
+     # fluidRow(column(12, verbatimTextOutput("states"))),
+     # fluidRow(column(12, verbatimTextOutput("IBUbins"))),
+     # fluidRow(column(12, verbatimTextOutput("ABVbins")))
+      #?fluidRow
     ),
     
     # Main panel for displaying outputs ----
@@ -50,7 +60,10 @@ ui <- fluidPage(
       # Output: Histogram ----
       plotOutput(outputId = "IBUplot"),
       plotOutput(outputId = "ABVplot"),
-      tableOutput('FileContents')
+      plotOutput(outputId = "ScatterPlot"),
+      plotOutput(outputId = "CityPlot"),
+      plotOutput(outputId = "BrewPlot")
+      #tableOutput("FileContents")
     )
   )
 )
